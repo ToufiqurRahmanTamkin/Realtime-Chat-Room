47fab0dc1f22ab4e895d6ba446c668a5d011ea8f
Toufiqur Rahman Tamkin <tamkin22rahman@gmail.com>
2021-08-15 02:39:41 +0600

    added messages

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from "react";
 import queryString from "query-string";
 import io from "socket.io-client";
-
 import TextContainer from "../TextContainer/TextContainer";
 import Messages from "../Messages/Messages";
 import InfoBar from "../InfoBar/InfoBar";
 import Input from "../Input/Input";
-
 import "./Chat.css";
 
 const ENDPOINT = "https://glacial-wildwood-38501.herokuapp.com/";
